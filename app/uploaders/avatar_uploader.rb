058615fdadd4879f18fb0a47c4232b9ca9287fda
Tatsuya Kawasaki <ta2ya.xxx@gmail.com>
2015-07-29 00:53:41 +0900

    change image gravity to resize

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -26,7 +26,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-  process :resize_to_fill => [150, 150]
+  process :resize_to_fill => [150, 150, :North]
   #
   # def scale(width, height)
   #   # do something
@@ -34,7 +34,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [50, 50]
+    process :resize_to_fill => [50, 50, :North]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
